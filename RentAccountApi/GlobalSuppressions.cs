8a8f2972a0b5f2e386e6449ab7bf8af116e76531
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-08-24 13:59:27 +0100

    Adding supression

---
+++ RentAccountApi/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "module")]
+[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:RentAccountApi.Startup.ConfigureDbContext(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:RentAccountApi.V1.Infrastructure.DynamoDBContext`1.Save(`0)~System.Threading.Tasks.Task")]
