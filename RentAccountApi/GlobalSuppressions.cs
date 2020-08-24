ba0e7b0fbb6978988ae6831c89c8a5abf2501696
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-08-24 15:35:38 +0100

    sorting issue with UK date.

--- RentAccountApi/GlobalSuppressions.cs
+++ RentAccountApi/GlobalSuppressions.cs
@@ -7,4 +7,6 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "module")]
 [assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:RentAccountApi.Startup.ConfigureDbContext(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
-[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:RentAccountApi.V1.Infrastructure.DynamoDBContext`1.Save(`0)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "module")]
+[assembly: SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "<Pending>", Scope = "member", Target = "~M:RentAccountApi.V1.Factories.AuditFactory.FormatToUkDate(System.DateTime)~System.DateTime")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "module")]
