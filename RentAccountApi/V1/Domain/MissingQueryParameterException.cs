8f6efd2cffcfa30ddbf503b110aed6f92e5d8e64
Matthew Keyworth <Matthew.Keyworth@hackney.gov.uk>
2020-08-26 10:18:40 +0100

    Added controller tests and exception handling to controller

---
+++ RentAccountApi/V1/Domain/MissingQueryParameterException.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentAccountApi.V1.Domain
+{
+    public class MissingQueryParameterException : Exception
+    {
+        public MissingQueryParameterException(string message)
+            :base(message)
+        { }
+    }
+}
